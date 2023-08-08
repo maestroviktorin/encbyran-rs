5bf900fa1b01fc37a99df455bce3863d549a96b8
Viktor Starodubtsev <viktorst2005@gmail.com>
2023-08-09 01:58:13 +0400

    Implementing of ActionVecs started. Modules fixed.

--- src/actions.rs
+++ src/actions.rs
@@ -1,51 +1,61 @@
-pub mod actions {
-    use rand::seq::SliceRandom;
-    use std::collections::HashSet;
+use rand::seq::SliceRandom;
+use std::collections::HashSet;
 
-    type ActionSet = HashSet<String>;
+pub struct ActionVecs {
+    plus: Vec<String>,
+    minus: Vec<String>,
+    new_line: Vec<String>,
+}
 
-    const ASCII_LETTERS: &str = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM";
+impl ActionVecs {
+    pub fn new(set_size: usize, word_max_size: usize) -> Self {
+        let mut plus: ActionSet = get_random_set(set_size, word_max_size);
+        let mut minus: ActionSet = get_random_set(set_size, word_max_size);
+        let mut new_line: ActionSet = get_random_set(set_size, word_max_size);
 
-    pub fn get_action_sets(set_size: usize, word_size: usize) -> (ActionSet, ActionSet, ActionSet) {
-        let mut action_plus: ActionSet = get_random_set(set_size, word_size);
-        let mut action_minus: ActionSet = get_random_set(set_size, word_size);
-        let mut action_new_line: ActionSet = get_random_set(set_size, word_size);
+        // Solution obtained from
+        // https://stackoverflow.com/questions/76860337/compound-hashset-operations-in-rust-or-how-to-get-an-explicit-difference-union
+        plus.retain(|word: &String| minus.remove(word) | new_line.remove(word) | true);
+        minus.retain(|word: &String| new_line.remove(word) | true);
 
-        // Solution obtained from https://stackoverflow.com/questions/76860337/compound-hashset-operations-in-rust
-        (action_plus, action_minus, action_new_line) = (
-            &(&action_plus - &action_minus) - &action_new_line,
-            &(&action_minus - &action_plus) - &action_new_line,
-            &(&action_new_line - &action_plus) - &action_minus,
+        Self {
+            plus: Vec::from_iter(plus.into_iter()),
+            minus: Vec::from_iter(minus.into_iter()),
+            new_line: Vec::from_iter(new_line.into_iter()),
+        }
+    }
+
+    // TODO: Implement random sampling from the requested actions vector.
+}
+
+type ActionSet = HashSet<String>;
+
+const ASCII_LETTERS: &str = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM";
+
+fn get_random_set(set_size: usize, word_max_size: usize) -> ActionSet {
+    let mut result: ActionSet = HashSet::new();
+
+    for _ in 0..set_size {
+        result.insert(get_random_word(word_max_size));
+    }
+
+    result
+}
+
+fn get_random_word(word_max_size: usize) -> String {
+    // https://docs.rs/rand/latest/rand/seq/index.html
+    let mut result: String = String::new();
+    let mut rng = rand::thread_rng();
+
+    for _ in 1..word_max_size {
+        result.push(
+            *ASCII_LETTERS
+                .to_string()
+                .into_bytes()
+                .choose(&mut rng)
+                .unwrap() as char,
         );
-
-        (action_plus, action_minus, action_new_line)
     }
 
-    fn get_random_set(set_size: usize, word_size: usize) -> ActionSet {
-        let mut result: ActionSet = HashSet::new();
-
-        for _ in 0..set_size {
-            result.insert(get_random_word(word_size));
-        }
-
-        result
-    }
-
-    fn get_random_word(word_size: usize) -> String {
-        // https://docs.rs/rand/latest/rand/seq/index.html
-        let mut result: String = String::new();
-        let mut rng = rand::thread_rng();
-
-        for _ in 1..word_size {
-            result.push(
-                *ASCII_LETTERS
-                    .to_string()
-                    .into_bytes()
-                    .choose(&mut rng)
-                    .unwrap() as char,
-            );
-        }
-
-        result
-    }
+    result
 }
--- src/cryptograph.rs
+++ src/cryptograph.rs
@@ -1,138 +1,131 @@
-pub mod cryptograph {
-    use rand::Rng;
-    use std::io::prelude::*;
-    use std::{
-        fs::{read_to_string, File},
-        path::Path,
+use rand::Rng;
+use std::io::prelude::*;
+use std::{
+    fs::{read_to_string, File},
+    path::Path,
+};
+
+pub fn cryptograph(
+    path_to_file_to_encrypt: &Path,
+    to_lower: bool,
+    rm_punctuation: bool,
+    shift: (usize, usize),
+    encrypted_new_line_length: (usize, usize),
+) {
+    // IMPORTANT: Investigate topic of functions allocation, traits and functions borrowing.
+    // INITIALLY PROVIDED BY: https://github.com/alexschrod (ilyvion on Discord).
+    let perform_to_lower: fn(&str) -> String = if to_lower {
+        |word: &str| word.to_lowercase()
+    } else {
+        |word: &str| word.to_string()
     };
 
-    pub fn cryptograph(
-        path_to_file_to_encrypt: &Path,
-        to_lower: bool,
-        rm_punctuation: bool,
-        shift: (usize, usize),
-        encrypted_new_line_length: (usize, usize),
-    ) {
-        // IMPORTANT: Investigate topic of functions allocation, traits and functions borrowing.
-        // INITIALLY PROVIDED BY: https://github.com/alexschrod (ilyvion on Discord).
-        let perform_to_lower: fn(&str) -> String = if to_lower {
-            |word: &str| word.to_lowercase()
-        } else {
-            |word: &str| word.to_string()
-        };
+    let perform_rm_punctuation: fn(String) -> String = if rm_punctuation {
+        cleared
+    } else {
+        |word: String| word.to_string()
+    };
 
-        let perform_rm_punctuation: fn(String) -> String = if rm_punctuation {
-            cleared
-        } else {
-            |word: String| word.to_string()
-        };
-
-        let origin_lines: Vec<Vec<String>> = read_to_string(&path_to_file_to_encrypt)
-            .unwrap()
-            .lines()
-            .map(|line: &str| {
-                line.trim()
-                    .split(" ")
-                    .map(perform_to_lower)
-                    .map(perform_rm_punctuation)
-                    .collect()
-            })
-            .collect();
-
-        let mut encrypted: File = get_file_bound_to(&path_to_file_to_encrypt, "encrypted-");
-        let mut decryptor: File = get_file_bound_to(&path_to_file_to_encrypt, "decryptor-for-");
-
-        for line in origin_lines.iter() {
-            encrypt_line(
-                line,
-                &mut encrypted,
-                &mut decryptor,
-                &shift,
-                &encrypted_new_line_length,
-            );
-        }
-    }
-
-    fn cleared(word: String) -> String {
-        let mut result: String = String::from(&word);
-        let mut n: usize = word.len() - 1;
-
-        while word.chars().nth(n).unwrap().is_ascii_punctuation() {
-            result.remove(n);
-            n -= 1;
-        }
-
-        result
-    }
-
-    fn get_file_bound_to(bound_to: &Path, beginning_name: &str) -> File {
-        File::create(format!(
-            "{beginning_name}{:?}{:?}",
-            bound_to.file_stem().unwrap().to_str(),
-            bound_to.extension().unwrap().to_str()
-        ))
+    let origin_lines: Vec<Vec<String>> = read_to_string(&path_to_file_to_encrypt)
         .unwrap()
+        .lines()
+        .map(|line: &str| {
+            line.trim()
+                .split(" ")
+                .map(perform_to_lower)
+                .map(perform_rm_punctuation)
+                .collect()
+        })
+        .collect();
+
+    let mut encrypted: File = get_file_bound_to(&path_to_file_to_encrypt, "encrypted-");
+    let mut decryptor: File = get_file_bound_to(&path_to_file_to_encrypt, "decryptor-for-");
+
+    for line in origin_lines.iter() {
+        encrypt_line(
+            line,
+            &mut encrypted,
+            &mut decryptor,
+            &shift,
+            &encrypted_new_line_length,
+        );
+    }
+}
+
+fn cleared(word: String) -> String {
+    let mut result: String = String::from(&word);
+    let mut n: usize = word.len() - 1;
+
+    while word.chars().nth(n).unwrap().is_ascii_punctuation() {
+        result.remove(n);
+        n -= 1;
     }
 
-    fn encrypt_line(
-        line: &Vec<String>,
-        encrypted: &mut File,
-        decryptor: &mut File,
-        shift: &(usize, usize),
-        encrypted_new_line_length: &(usize, usize),
-    ) {
-        for word in line {
-            encrypt_word(word, encrypted, decryptor, shift);
+    result
+}
+
+fn get_file_bound_to(bound_to: &Path, beginning_name: &str) -> File {
+    File::create(format!(
+        "{beginning_name}{:?}{:?}",
+        bound_to.file_stem().unwrap().to_str(),
+        bound_to.extension().unwrap().to_str()
+    ))
+    .unwrap()
+}
+
+fn encrypt_line(
+    line: &Vec<String>,
+    encrypted: &mut File,
+    decryptor: &mut File,
+    shift: &(usize, usize),
+    encrypted_new_line_length: &(usize, usize),
+) {
+    for word in line {
+        encrypt_word(word, encrypted, decryptor, shift);
+    }
+
+    // Encrypt the new line notation using `encrypted_new_line_length`.
+}
+
+fn encrypt_word(word: &str, encrypted: &mut File, decryptor: &mut File, shift: &(usize, usize)) {
+    let mut rng: rand::rngs::ThreadRng = rand::thread_rng();
+
+    for byte in word.as_bytes() {
+        let (key, action) = (rng.gen_range(shift.0..shift.1) as isize, rng.gen_bool(0.5));
+        write!(decryptor, "{}", key.to_string()).unwrap();
+
+        // TODO: Add 'action-words' written into `encrypted`.
+        if action {
+            write!(encrypted, "{}", (*byte as isize + key).to_string()).unwrap();
+        } else {
+            write!(encrypted, "{}", (*byte as isize - key).to_string()).unwrap();
         }
-
-        // Encrypt the new line notation using `encrypted_new_line_length`.
     }
 
-    fn encrypt_word(
-        word: &str,
-        encrypted: &mut File,
-        decryptor: &mut File,
-        shift: &(usize, usize),
-    ) {
-        let mut rng: rand::rngs::ThreadRng = rand::thread_rng();
+    write!(encrypted, "\n").unwrap();
+    write!(decryptor, "\n").unwrap();
+}
 
-        for byte in word.as_bytes() {
-            let (key, action) = (rng.gen_range(shift.0..shift.1) as isize, rng.gen_bool(0.5));
-            write!(decryptor, "{}", key.to_string()).unwrap();
+fn get_encrypted_new_line(
+    encrypted_new_line_length: &(usize, usize),
+    shift: &(usize, usize),
+) -> String {
+    // Build an encrypted newline by joining some 'units'.
+    String::from("")
+}
 
-            // TODO: Add 'action-words' written into `encrypted`.
-            if action {
-                write!(encrypted, "{}", (*byte as isize + key).to_string()).unwrap();
-            } else {
-                write!(encrypted, "{}", (*byte as isize - key).to_string()).unwrap();
-            }
-        }
+fn get_encrypted_new_line_unit(shift: &(usize, usize)) -> String {
+    // Produce a unit of an encrypted new line.
+    String::from("")
+}
 
-        write!(encrypted, "\n").unwrap();
-        write!(decryptor, "\n").unwrap();
-    }
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-    fn get_encrypted_new_line(
-        encrypted_new_line_length: &(usize, usize),
-        shift: &(usize, usize),
-    ) -> String {
-        // Build an encrypted newline by joining some 'units'.
-        String::from("")
-    }
-
-    fn get_encrypted_new_line_unit(shift: &(usize, usize)) -> String {
-        // Produce a unit of an encrypted new line.
-        String::from("")
-    }
-
-    #[cfg(test)]
-    mod tests {
-        use super::*;
-
-        #[test]
-        fn test_cleared() {
-            assert_eq!(cleared("Hello!".to_string()), "Hello".to_string());
-            assert_eq!(cleared("Hello...".to_string()), "Hello".to_string());
-        }
+    #[test]
+    fn test_cleared() {
+        assert_eq!(cleared("Hello!".to_string()), "Hello".to_string());
+        assert_eq!(cleared("Hello...".to_string()), "Hello".to_string());
     }
 }
